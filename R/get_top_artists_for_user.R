dc3f852f75c73ca99f871f726278ff6fabaa7994
danraff <d.raff@alumni.ubc.ca>
2018-04-12 13:20:16 -0700

    Roxygenize

--- R/get_top_artists_for_user.R
+++ R/get_top_artists_for_user.R
@@ -1,3 +1,28 @@
+#' get_top_artists_for_user: Get the top artists for the user
+#' @importFrom glue httr jsonlite dplyr
+#'
+#' @param user_auth_token: a string corresponding to a valid Spotify API user authentication token
+#' @param limit_num: an integer between 1 and 50
+#' @param time_range_opt: an integer, either 1,2 or 3.
+#'      1 = `long_term` is last several years of data
+#'      2 = `medium_term` is last 6 months
+#'      3 = `short_term` last 4 weeks
+#'
+#' @return a dataframe of artist name, popularity, genres, and followers
+#'      name = character
+#'      popularity = integer between 1 and 100 corresponding to popularity
+#'      genres = character vector of genres associated with artists
+#'      followers = integer of number of followers of artist
+#'
+#' @export
+#'
+#' @examples
+#' my_auth_token <- "BQDk6Yebn4V94Cbd121Rs73_q4Vtx1WotIX7o3spa5THlzfZi1pQn-jywCkz2XNld5Pa1H3jxo7EGL8JZ-1_G28Sg5as5ZaBeIfKCngrvoT52sLhkUYbplpgk6_G7MIyByENCv32oJAR4txcWgI0j5Bewds-cvR8foos2fz71Xob"
+#' my_df <- get_top_artists_for_user(my_auth_token, 50, 2)
+
+
+
+
 get_top_artists_for_user <- function(user_auth_token, limit_num, time_range_opt) {
 
   base_url <- "https://api.spotify.com"
@@ -7,30 +32,25 @@ get_top_artists_for_user <- function(user_auth_token, limit_num, time_range_opt)
   limit <- glue("&limit=",limit_num)
   time_range_options = c("long_term", "medium_term", "short_term")
   time_range <- glue("time_range=", time_range_options[2])
-  
-  # Where:
-  ## `long_term` is last several years of data
-  ## `medium_term` is last 6 months
-  ## `short_term` last 4 weeks  
-  
+
   auth_token <- user_auth_token
-  
+
   header_auth <- add_headers(c(Authorization = glue("Bearer ", auth_token)))
-  
-  request <- glue(base_url, "/v1", "/", 
+
+  request <- glue(base_url, "/v1", "/",
                   "{instruction}","/",
-                  "{type}", 
-                  "?", 
-                  time_range, 
+                  "{type}",
+                  "?",
+                  time_range,
                   limit)
-  
-  json_obj <- GET(request, 
+
+  json_obj <- GET(request,
       add_headers(c(Authorization = glue("Bearer ", auth_token))))
-  
-  df_obj <- fromJSON(json_obj %>% content("text")) %>% 
-    .$items %>% 
-    flatten() %>% 
-    select(name, popularity, genres, followers.total) %>% 
+
+  df_obj <- fromJSON(json_obj %>% content("text")) %>%
+    .$items %>%
+    flatten() %>%
+    select(name, popularity, genres, followers.total) %>%
     rename(followers = "followers.total")
   df_obj
 
