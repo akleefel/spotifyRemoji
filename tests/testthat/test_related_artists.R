b5590e8dfd98eff8376f1779e0aaa3e36bee771d
danraff <d.raff@alumni.ubc.ca>
2018-04-14 10:48:04 -0700

    context instead of importing libraries

--- tests/testthat/test_related_artists.R
+++ tests/testthat/test_related_artists.R
@@ -1,14 +1,10 @@
-library(httr)
-library(jsonlite)
-library(dplyr)
-library(glue)
+context("related_artists test")
 
 
-
-#Test A 
+#Test A
 
 test_that("Passing valid authentication and artist name leads to correct shape of dataframe output"){
-  
+
   expect_identical(dim(related_artists(auth, "haftbefehl"))[1],20)
 }
 
@@ -16,14 +12,14 @@ test_that("Passing valid authentication and artist name leads to correct shape o
 #Test B
 
 test_that("Passing invalid artist name leads to empty dataframe"){
-  
+
   expect_identical(dim(related_artists(auth, "Hafti"))[1],0)
 }
 
 #Test C
 
 test_that("Passing invalid authentication leads to empty dataframe"){
-  
+
   auth <- "hello"
   expect_identical(dim(related_artists(auth, "Haftbefehl"))[1],0)
 }
@@ -33,7 +29,7 @@ test_that("Passing invalid authentication leads to empty dataframe"){
 
 #Test B
 test_that("Both arguments were passed", {
-  
+
   expect_error(related_artists(user_auth_token = auth, artistName = ""), "One of 'user_auth_token' or 'artistName' is missing")
 })
 
